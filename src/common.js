20f9e8946de3747fc1bbb6598db43a83f1b509e4
artempelenev <pstemka@gmail.com>
2020-02-19 16:18:08 +0100

    cleanup repo

--- src/common.js
+++
@@ -1,5 +0,0 @@
-require("./stylesheets/base.scss");
-require("./images/tech-radar-landing-page-wide.png");
-require("./images/intracto_logo.jpg");
-require("./images/favicon.ico");
-require("./images/search-logo-2x.svg");
