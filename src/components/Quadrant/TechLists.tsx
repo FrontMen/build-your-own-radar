d0605f2f8cb72ea1e1c811115216b2ff441989b9
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2020-03-10 15:34:44 +0100

    style(Rings): Add blue dot and a tooltip

--- src/components/Quadrant/TechLists.tsx
+++ src/components/Quadrant/TechLists.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { Typography } from 'src/Theme/Typography';
 import { MediaQueries } from 'src/Theme/Helpers';
@@ -35,6 +35,60 @@ const Title = styled.h3`
 const List = styled.ul`
   padding-left: 0;
   margin: 0;
+`;
+
+const Dot = styled.span`
+  color: #1ebccd;
+  padding-right: 4px;
+`;
+
+const ToolTip = styled.span`
+  background-color: #bfc0bf;
+  position: relative;
+  display: inline-block;
+  line-height: 22px;
+  width: 22px;
+  margin-left: 12px;
+  color: #fff;
+  cursor: pointer;
+  text-align: center;
+  border-radius: 50%;
+  font-size: 16px;
+
+  &:hover {
+    .tooltip-text {
+      visibility: visible;
+      opacity: 1;
+    } 
+  }
+
+  .tooltip-text {
+    visibility: hidden;
+    position: absolute;
+    font-size: 12px;
+    width: 270px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    padding: 5px;
+    z-index: 1;
+    bottom: 130%;
+    margin-left: -80px;
+    opacity: 1;
+    transition: opacity 0.3s;
+
+    &::after {
+      content: "";
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      margin-left: -64px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: #555 transparent transparent transparent;
+    }
+  }
 `;
 
 export interface TechnologiesListProps {
@@ -77,7 +131,16 @@ export const TechLists: React.FC<TechnologiesListProps> = ({
     <Section>
       {Object.entries(data).map(([ring, technologiesInRing]) => (
         <Ring key={ring}>
-          <Title>{ring}</Title>
+          <Title>
+            <Dot>●</Dot>
+            {ring}
+            <ToolTip>?
+              <span className="tooltip-text">
+                Worth pursuing. It is important to understand how to build up this capability. 
+                Entreprises should try this technology on a project that can handle the risk
+              </span>
+            </ToolTip>
+          </Title>
           <List>
             {technologiesInRing.map(technology => (
               <TechItem
