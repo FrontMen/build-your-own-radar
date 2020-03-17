e0631c95ff7e68b387f07d0d2932b47c53532319
artempelenev <pstemka@gmail.com>
2020-03-17 11:19:17 +0100

    add e2e for quadrant p1

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -123,7 +123,7 @@ export const Search: React.FC<SearchProps> = ({ setHighlighted }) => {
   );
 
   return (
-    <Container>
+    <Container data-testid="subnav-search-container">
       <InputContainer>
         <Input
           data-testid="search-input"
