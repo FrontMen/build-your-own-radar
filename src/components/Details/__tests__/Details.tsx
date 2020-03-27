e5f0d1fcbadd980a42c226d38ecbe11dfe15c2ca
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2020-03-27 17:28:12 +0100

    chore(tsconfig): include test folder into src folder

--- src/components/Details/__tests__/Details.tsx
+++ src/components/Details/__tests__/Details.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Details from '../Details';
-import { withAllProviders } from '../../../../test/helpers';
+import { withAllProviders } from 'test/helpers';
 
 const TEST_ID = 'testId';
 
