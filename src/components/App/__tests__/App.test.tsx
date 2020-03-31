497278e7bc62f638f3e7a15d9d5007bd72c49186
artempelenev <pstemka@gmail.com>
2020-03-31 21:31:00 +0200

    remove error, should trigger deploy now

--- src/components/App/__tests__/App.test.tsx
+++ src/components/App/__tests__/App.test.tsx
@@ -6,7 +6,7 @@ describe('App', () => {
   const appWrapper = shallow(<App />);
 
   it('should wrap content into redux and theme providers', () => {
-    expect(appWrapper.find('Provider1')).toHaveLength(1);
+    expect(appWrapper.find('Provider')).toHaveLength(1);
     expect(appWrapper.find('ThemeProvider')).toHaveLength(1);
   });
 });
