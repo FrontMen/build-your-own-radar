a88b1a59868fe712735ae3bad657e27965042749
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2020-04-10 16:19:24 +0200

    feat(DataFilter): add click outside listener

--- src/components/DataSetFilter/index.tsx
+++ src/components/DataSetFilter/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectedDataSetSelector } from 'redux/selectors/filters';
 import { filtersActions } from 'redux/actions/filters';
@@ -16,19 +16,23 @@ export const DataSetFilter = () => {
   const [opened, setOpened] = useState<boolean>(false);
   const { availableDates, selected } = useSelector(selectedDataSetSelector);
   const dispatch = useDispatch();
-  const selectDate = useCallback(
-    (date: string) => {
-      dispatch(filtersActions.selectDataSet(date));
-    },
-    [dispatch],
-  );
-  const toggleOpened = useCallback(() => {
-    setOpened(prevOpened => !prevOpened);
-  }, [setOpened]);
+  const filterContainer: any = useRef(null);
+  const toggleOpened = (opened: boolean) => setOpened.bind(null, opened);
+
+  useEffect(() => {
+    const clickHandler = (e: any) => {
+      if (!filterContainer.current?.contains(e.target)) setOpened(false);
+    };
+    document.addEventListener('click', clickHandler);
+    return () => document.removeEventListener('click', clickHandler);
+  }, []);
 
   return (
-    <Container data-testid="dataSetFilter-container">
-      <Selected data-testid="dataSetFilter-anchor" onClick={toggleOpened}>
+    <Container data-testid="dataSetFilter-container" ref={filterContainer}>
+      <Selected
+        data-testid="dataSetFilter-anchor"
+        onClick={toggleOpened(!opened)}
+      >
         <Text data-testid="dataSetFilter-text">{dateFormat(selected)}</Text>
         <Triangle
           opened={opened}
@@ -48,7 +52,7 @@ export const DataSetFilter = () => {
               data-testid={`dataSetFilter-dropdown-option-${date}`}
               selected={selected === date}
               onClick={() => {
-                selectDate(date);
+                dispatch(filtersActions.selectDataSet(date));
                 setOpened(false);
               }}
             >
