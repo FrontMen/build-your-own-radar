e361c2862cf8370ad6bf4d5f501f637e203eb58b
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2020-05-06 11:32:21 +0200

    feat(Apollo): add typedefs

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -2,6 +2,40 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 
+const typeDefs = `
+  type Query {
+    technologies: [Technology]
+  }
+
+  type Technology {
+    name: String!
+    description: String
+    isNew: Boolean
+    published_at: Date
+    inRadar: Boolean
+    quadrant: Quadrant
+    ring: Ring
+    companies: [Company]
+  }
+
+  type Ring {
+    name: String!
+    color: String!
+    backgroundColor: String!
+    order: Int
+  }
+
+  type Quadrant {
+    name: String!
+    color: String!
+  }
+
+  type Company {
+    name: String!
+    shortName: String!
+  }
+
+`;
 const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
@@ -10,4 +44,5 @@ const link = new HttpLink({
 export const client = new ApolloClient<NormalizedCacheObject>({
   cache,
   link,
+  typeDefs,
 });
