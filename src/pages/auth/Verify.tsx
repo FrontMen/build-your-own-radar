46c5dc7a33a7ed1f93cf22325d01eeec9688d54f
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2020-05-16 12:30:33 +0200

    feat(auth): add Home Skeleton to the verify page

--- src/pages/auth/Verify.tsx
+++ src/pages/auth/Verify.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { HomePageSkeleton } from 'components/Skeleton/Homepage';
 import { RouteComponentProps } from 'react-router-dom';
 import { authActions } from 'redux/actions/auth';
 
@@ -13,5 +14,5 @@ export const Verify: React.FC<RouteComponentProps> = props => {
     );
   }, [dispatch, location.search, match.params]);
 
-  return <div>Yo Homie</div>;
+  return <HomePageSkeleton />;
 };
