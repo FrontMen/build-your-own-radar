9df4f237b80a4e550896513450fd26705212f5ad
artempelenev <pstemka@gmail.com>
2020-06-09 16:02:00 +0200

    moved data loading to main

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,9 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { auth } from 'utils/auth';
-import { useDispatch, useSelector } from 'react-redux';
-import { technologiesLoadingStateSelector } from 'redux/selectors/technologies';
-import { actions } from 'redux/actions';
 
 interface PrivateProps {
   component: React.ElementType;
@@ -14,31 +11,20 @@ interface PrivateProps {
 export const PrivateRoute: React.FC<PrivateProps> = ({
   component: Component,
   ...rest
-}) => {
-  const { initialized, loading } = useSelector(
-    technologiesLoadingStateSelector(),
-  );
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (!initialized && !loading && auth.isLoggedIn()) {
-      dispatch(actions.fetchTechnologies());
+}) => (
+  <Route
+    {...rest}
+    render={props =>
+      auth.isLoggedIn() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/auth/login',
+            state: { from: props.location },
+          }}
+        />
+      )
     }
-  }, [initialized, loading, dispatch]);
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        auth.isLoggedIn() ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: '/auth/login',
-              state: { from: props.location },
-            }}
-          />
-        )
-      }
-    />
-  );
-};
+  />
+);
